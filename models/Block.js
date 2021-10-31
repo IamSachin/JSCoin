50c1ea4a2c185e677a8b9a0a2592542aa02ac343
Sachin Rajput <sachin.rajput.k@gmail.com>
2021-10-31 20:16:59 +0530

    Initial commit

---
+++ models/Block.js
@@ -0,0 +1,47 @@
+import { mode, SHA256 } from 'crypto-js';
+
+export class Block {
+  /**
+   * The position of the block on the chain
+   * @var {int}
+   */
+  index;
+
+  /**
+   * @var {int}
+   */
+  timestamp;
+
+  /**
+   * @var {object}
+   */
+  data;
+
+  /**
+   * @var {string}
+   */
+  hash;
+
+  /**
+   * Hash of the block before the current block
+   * @var {string}
+   */
+  previousHash;
+
+  constructor(model = {}) {
+    this.index = model.index;
+    this.timestamp = model.timestamp;
+    this.data = model.data;
+    this.hash = model.hash ?? '';
+    this.previousHash = model.previousHash;
+  }
+
+  calculateHash() {
+    return SHA256(
+      `${this.index} \
+      + ${this.timestamp} \
+      + ${this.previousHash} \
+      + ${JSON.stringify(this.data)}`
+    ).toString();
+  }
+}
---
+++ models/Blockchain.js
@@ -0,0 +1,49 @@
+import { Block } from './Block';
+
+export class Blockchain {
+  /**
+   * @var {Block[]}
+   */
+  chain;
+
+  constructor(model = {}) {
+    this.chain = [this.#createGenesisBlock()];
+  }
+
+  /**
+   * @returns {Block}
+   */
+  getLatestBlock() {
+    this.chain.slice(-1);
+  }
+
+  /**
+   * @param {Block} block
+   * @returns {Block}
+   */
+  addBlock(block) {
+    this.chain.push(this.#processNewBlock(block));
+  }
+
+  /**
+   * @param {Block} block
+   * @returns {Block}
+   */
+  #processNewBlock(block) {
+    block.previousHash = this.getLatestBlock().hash;
+    block.hash = block.calculateHash();
+    return block;
+  }
+
+  /**
+   * @returns {Block}
+   */
+  #createGenesisBlock() {
+    return new Block({
+      index: 0,
+      timestamp: +new Date(),
+      data: 'Genesis Block',
+      previousHash: '0'
+    });
+  }
+}
